a770c0ea29d1f9fd2b21ff719a9cf4cc42c02da4
opilane <opilane@HP-CZC2349HTY>
2025-09-05 11:40:58 +0300

    Ill have to recheck everything and whacth some youtube tutorials 3_:

--- Osa1_funktisoonid.cs
+++ Osa1_funktisoonid.cs
@@ -10,8 +10,121 @@ namespace TARpv24Ckeel
     {
         public static float Kalkulator(float arv1, float arv2)
         {
-            float k=arv1*arv2;
+            float k = arv1 * arv2;
             return k;
+        }
+        public static string Kuu_nimetus(int kuu_nr)
+        {
+            string kuu = "";
+            switch (kuu_nr)
+            {
+                case 1: kuu = "Jaanuar"; break;
+                case 2: kuu = "Veebruar"; break;
+                case 3: kuu = "Märts"; break;
+                case 4: kuu = "Aprill"; break;
+                case 5: kuu = "Mai"; break;
+                case 6: kuu = "Juuni"; break;
+                case 7: kuu = "Juuli"; break;
+                case 8: kuu = "August"; break;
+                case 9: kuu = "September"; break;
+                case 10: kuu = "Oktoober"; break;
+                case 11: kuu = "November"; break;
+                case 12: kuu = "Detsember"; break;
+                default:
+                    kuu = "???"; Console.WriteLine("nuh uh");
+                    break;
+            }
+            return kuu;
+        }
+
+        public static string Hooaeg(int kuu_nr)
+        {
+            string hoo = "";
+            if (kuu_nr == 1 || kuu_nr == 2 || kuu_nr == 12) //&& - and. || - or
+            {
+                hoo = "Talv";
+            }
+            else if (kuu_nr > 2 && kuu_nr < 6)
+            {
+                hoo = "Kevad";
+            }
+
+            else if (kuu_nr > 5 && kuu_nr < 9)
+            {
+                hoo = "Suvi";
+            }
+
+            else if (kuu_nr > 8 && kuu_nr < 12) //9,10,11
+            {
+                hoo = "Sügis";
+            }
+            else
+            {
+                hoo = "???";
+            }
+            return hoo;
+        }
+
+        public static string KinoPilet (int aastat)
+        {
+            string kusi= "";
+            if (aastat < 6)
+            {
+                kusi = "Tasuta";
+            }
+
+            else if (aastat >= 6 && aastat <= 14)
+            {
+                kusi = "Lastepilet";
+            }
+
+            else if (aastat >= 15 && aastat <= 65)
+            {
+                kusi = "täispilet";
+            }
+
+            else if (aastat >= 65 && aastat <= 100)
+            {
+                kusi = "sooduspilet";
+            }
+
+            else if (aastat < 0 || aastat > 100)
+            {
+                kusi = "viga";
+            }
+
+            else 
+            {
+                kusi = "thats it, Im stealing your cookies >:3";
+            }
+            return kusi;
+        }
+
+        //public static string Pingi (string nimi1, string nimi2) 
+        //{
+        //    string neig = "";
+        //    int vastunud = 0;
+
+        //    if (nimi1 == "Maksim")
+        //    {
+        //        neig = "Ja";
+        //    }
+
+        //    else if (nimi2 == "Nikita")
+        //    {
+        //        neig = "Ja";
+        //    }
+        //    else
+        //    {
+        //        neig = "thats it, Im stealing your cookies >:3";
+        //    }
+        //    return neig;
+        //}
+    
+        public static float HindTrio (float a, float b)
+        {
+            float formula = a * b;
+            return formula;
         }
     }
 }
--- StartClass.cs
+++ StartClass.cs
@@ -12,34 +12,129 @@ namespace TARpv24Ckeel
 
         public static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-
-            Console.WriteLine("Tere tulemast! Mis on sinu nimi?");
-            string tekst=Console.ReadLine();
-            Console.WriteLine($"{tekst}, Rööm näha"); // $ is like f in python
-            int a = 1000;
-            char taht = 'A'; // char is for single characters
-            Console.Write($"Esimene arv on {a}, Siesta b =...");
-            int b = int.Parse(Console.ReadLine());
-            Console.WriteLine("Esimene arv on {0}, Siesta {1} summa on {2}", a,b,a+b);
-            Console.WriteLine("Ujukomaarv");
-            double d=double.Parse(Console.ReadLine());
-            Console.WriteLine(d);
-            float f=float.Parse(Console.ReadLine());
-            Console.WriteLine(f);
-            bool t = true;
-
-            Console.Clear();
-            Console.WriteLine("welcome to the calculator :3");
-
             Random rnd = new Random();
-            a = rnd.Next(-100, 200);
-            Console.WriteLine(a);
-            float vastus=Osa1_funktisoonid.Kalkulator(f,a);
-            Console.WriteLine($"Kalkulaatori tulemus : {vastus}");
+            //2. Osa Valikud
+            int kuu_number = rnd.Next(1, 12);
+            string nimetus = Osa1_funktisoonid.Kuu_nimetus(kuu_number);
+            Console.WriteLine($"Nr: {kuu_number}-{nimetus}");
+
+            Console.WriteLine("Kas tahad dekooreerida arv-->nimitusse");
+            string vastus = Console.ReadLine();
+            if (vastus.ToLower() != "jah")
+            {
+                Console.WriteLine("Ei taha, siis ei taha");
+            }
+            else
+            {
+                try
+                {
+                    Console.Write("Nr: ");
+                    kuu_number = int.Parse(Console.ReadLine());
+                    Console.WriteLine(Osa1_funktisoonid.Hooaeg(kuu_number));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+            }
+
+            // harjutus 1
+            Console.WriteLine("Mul on amnesia aga ma arvan, et sa oled Juku. Kas ma olen õige?");
+            string answer = Console.ReadLine();
+            if (answer.ToLower() != "jah" && answer.ToLower() != "ja")
+            {
+                Console.WriteLine("awh, 3:");
+            }
+            else
+            {
+                try
+                {
+                    Console.WriteLine("Kui vana sa oled? (bro think he is in disco elysium)");
+                    int answer2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine(Osa1_funktisoonid.KinoPilet(answer2));
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            // harjutus 2
+            Console.WriteLine("Mul on amnesia aga ma arvan (again), et nad on pinginaabrid? Miliseid neda nimid on?");
+            string tablets0 = Console.ReadLine();
+            Console.WriteLine("ja...");
+            string tablets1 = Console.ReadLine();
+            if (tablets0 != "Maksim" || tablets1 != "Nikita")
+            {
+                Console.WriteLine("Brotha, do you have amnesia as well?");
+            }
+            else
+            {
+                try
+                {
+                    Console.WriteLine("Oh... Tanan!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            Console.WriteLine("Yo, kas te tahate suurem ruum?");
+            float priceforpetemeeter = 20;
+            string BOBTHEBUILDER = Console.ReadLine();
+            if (BOBTHEBUILDER != "jah")
+            {
+                Console.WriteLine("Ma olen väga kurb... 3_;");
+            }
+            else
+            {
+                try
+                {
+                    Console.WriteLine("Kui Palju?");
+                    float a = float.Parse(Console.ReadLine());
+                    float b = float.Parse(Console.ReadLine());
+                    float formula = a*b;
+                    Console.WriteLine(Osa1_funktisoonid.HindTrio(a, b));
+                    Console.WriteLine($"Te peate makstaa {formula*priceforpetemeeter}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e); 
+                }
+            }
+
+
+            //I.Osa. Andmetüübid, Alamfunktsioon
+            //    Console.OutputEncoding = Encoding.UTF8;
+
+            //    Console.BackgroundColor = ConsoleColor.Green;
+            //    Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            //    Console.WriteLine("Tere tulemast! Mis on sinu nimi?");
+            //    string tekst=Console.ReadLine();
+            //    Console.WriteLine($"{tekst}, Rööm näha"); // $ is like f in python
+            //    int a = 1000;
+            //    char taht = 'A'; // char is for single characters
+            //    Console.Write($"Esimene arv on {a}, Siesta b =...");
+            //    int b = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("Esimene arv on {0}, Siesta {1} summa on {2}", a,b,a+b);
+            //    Console.WriteLine("Ujukomaarv");
+            //    double d=double.Parse(Console.ReadLine());
+            //    Console.WriteLine(d);
+            //    float f=float.Parse(Console.ReadLine());
+            //    Console.WriteLine(f);
+            //    bool t = true;
+
+            //    Console.Clear();
+            //    Console.WriteLine("welcome to the calculator :3");
+
+            //    Random rnd = new Random();
+            //    a = rnd.Next(-100, 200);
+            //    Console.WriteLine(a);
+            //    float vastus=Osa1_funktisoonid.Kalkulator(f,a);
+            //    Console.WriteLine($"Kalkulaatori tulemus : {vastus}");
         }
     }
 }
